9f8b5df18baa90553f8c593399d64839b1780ca2
Julio Cartier <juliocartier@gmail.com>
2018-09-15 10:20:50 -0300

    Alteracao

--- www/hello.js
+++ www/hello.js
@@ -1,5 +1,7 @@
 /*global cordova, module*/
 
+var exec = require('cordova/exec');
+
 module.exports = {
     greet: function (name, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
